fe977b68277b744229e1195f2d0663edafaa25e6
Flavius Condurache <flavius@drcarmen.com>
2022-02-02 15:21:55 +0200

    Lesson 4: Subroutines

--- Assembly/hello-world-len.asm
+++ Assembly/hello-world-len.asm
@@ -11,9 +11,23 @@ global  _start
 
 _start:
 
-    mov ebx, msg        ; move the address of our message string into EBX
-    mov eax, ebx        ; move the address in EBX into EAX as well (Both now point to the same segment in memory)
+    mov eax, msg        ; move the address of our message string into EAX
+    call strlen         ; call our function to calculate the length of the string
 
+    mov edx, eax        ; our function leaves the result in EAX
+    mov ecx, msg
+    mov ebx, 1
+    mov eax, 4
+    int 80h
+
+    mov ebx, 0
+    mov eax, 1
+    int 80h
+
+strlen:                 ; this is our first function declaration
+    push ebx            ; push the value in EBX onto the stack to preserve it while we use EBX in this function
+    mov ebx, eax        ; move the address in EAX into EBX (Both point to the same segment in memory)
+ 
 nextchar:
     cmp byte[eax], 0    ; compare the byte pointed to by EAX at this address against zero (Zero is an end of string delimiter)
     jz  finished        ; jump (if the zero flagged has been set) to the point in the code labeled 'finished'
@@ -27,12 +41,5 @@ finished:
                         ; when you subtract one memory address from another of the same type
                         ; the result is number of segments between them - in this case the number of bytes
 
-    mov edx, eax        ; EAX now equals the number of bytes in our string
-    mov ecx, msg        ; the rest of the code should be familiar now
-    mov ebx, 1
-    mov eax, 4
-    int 80h
-
-    mov ebx, 0
-    mov eax, 1
-    int 80h
+    pop ebx             ; pop the value on the stack back into EBX
+    ret                 ; return to where the function was called
