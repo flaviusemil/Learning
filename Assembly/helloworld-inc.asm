8288e857ca80497704600f25ef12f4b48a485371
Flavius Condurache <flavius@drcarmen.com>
2022-02-02 15:37:48 +0200

    Lesson 6: NULL terminating bytes

--- Assembly/helloworld-inc.asm
+++ Assembly/helloworld-inc.asm
@@ -3,11 +3,11 @@
 ; Link with (64 bit systems require elf_i386 option): ld -m elf_i386 helloworld-inc.o -o helloworld-inc
 ; Run with: ./helloworld-inc
 
-%include 'functions.asm'                                ; include our external file
+%include 'functions.asm'
 
 SECTION .data
-msg1    db  'Hello, brave new world!', 0Ah              ; our first message string
-msg2    db  'This is how we recycle in NASM.', 0Ah      ; our second message string
+msg1    db  'Hello, brave new world!', 0Ah, 0h              ; NOTE the null terminating byte
+msg2    db  'This is how we recycle in NASM.', 0Ah, 0h      ; NOTE the null terminating byte
 
 SECTION .text
 global _start
