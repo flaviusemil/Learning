5db337ca0793b301241df5658cef0f2caa72ebc7
Flavius Condurache <flavius@drcarmen.com>
2022-02-02 15:52:08 +0200

    Lesson 7: Linefeeds

--- Assembly/helloworld-inc.asm
+++ Assembly/helloworld-inc.asm
@@ -6,15 +6,15 @@
 %include 'functions.asm'
 
 SECTION .data
-msg1    db  'Hello, brave new world!', 0Ah, 0h              ; NOTE the null terminating byte
-msg2    db  'This is how we recycle in NASM.', 0Ah, 0h      ; NOTE the null terminating byte
+msg1    db  'Hello, brave new world!', 0h              ; NOTE we have removed the line feed character 0Ah
+msg2    db  'This is how we recycle in NASM.', 0h      ; NOTE we have removed the line feed character 0Ah
 
 SECTION .text
 global _start
 
 _start:
     mov eax, msg1
-    call sprint
+    call sprintLF
     mov eax, msg2
-    call sprint
+    call sprintLF
     call quit
